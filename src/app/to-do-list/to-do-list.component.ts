743fcef935dd59ced7a6c3c81be2740f3e23d77c
Kevin <matsekekk@gmail.com>
2025-01-30 12:58:45 +0200

    Adding forms, add, and delete from array

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,27 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'to-do-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.css'
 })
 export class ToDoListComponent {
 
-  TodoList: any[] = [
+   TodoList: any[] = [
     { Task: "Laundry", Status: "Completed" },
     { Task: "Visit", Status: "Completed" },
     { Task: "Work", Status: "In-progress" },
     { Task: "Code", Status: "To-do" }
   ];
 
-  constructor() {
+  todoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      Task: [''],
+      Status: [''],
+    });
     this.GetAllToDoListItems();
   }
 
   GetAllToDoListItems(): any[] {
     return this.TodoList;
+  }
+
+  AddToDoLIstItem() {
+    const newItem = this.todoForm.value;
+    this.TodoList.push(newItem);
+  }
+
+  RemoveToDoLIstItem(itemToRemove: any) {
+    const index = this.TodoList.findIndex(item => item === itemToRemove && item.Task === itemToRemove.Task);
+
+    if (index !== -1) {
+      this.TodoList.splice(index, 1);
+    }
   }
 }
